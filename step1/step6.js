358da792895bb5ae54eb1f1471039cd15d45ae82
Lwong01 <43592585+Lwong01@users.noreply.github.com>
2019-01-20 22:34:01 -0800

    Create step6.js

---
+++ step1/step6.js
@@ -0,0 +1,27 @@
+var getHTML = require('./http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printLowerCase(html) {
+  console.log(html.toLowerCase());
+
+
+}
+function printReverse(html) {
+  var reversePrint = html.split("").reverse().join("")
+  console.log(reversePrint);
+
+
+}
+
+function printUpperCase(html) {
+  console.log(html.toUpperCase());
+
+
+}
+
+getHTML(requestOptions, printLowerCase);
+
