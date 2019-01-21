1ea507e4c19cf6837aa64908e0033c6dfcd95af4
Lwong01 <43592585+Lwong01@users.noreply.github.com>
2019-01-20 22:29:11 -0800

    Create Step2.js

---
+++ step1/Step2.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+function getAndPrintHTML() {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf-8');
+
+    var buffer = "";
+    response.on('data', function (chunk) {
+      buffer += chunk;
+    });
+    response.on('end', function () {
+      console.log(buffer);
+  })
+
+  })
+
+};
+
+getAndPrintHTML();
