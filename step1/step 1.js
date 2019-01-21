f15d8ab90bca19533979644628dc6298e35249c3
Lwong01 <43592585+Lwong01@users.noreply.github.com>
2019-01-20 22:28:48 -0800

    Create step 1.js

---
+++ step1/step 1.js
@@ -0,0 +1,19 @@
+var https = require('https');
+function getAndPrintHTMLChunks() {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+      console.log('Chunk Received:', chunk, "\n");
+    });
+  })
+};
+
+getAndPrintHTMLChunks();
+
+
